9225bb0f267ad69d34b590dcff513d9607bd595d
Nauder <prestes.nauder@gmail.com>
2025-12-28 20:29:48 -0300

    feat: add UnityPy path variable to build script

--- build.ps1
+++ build.ps1
@@ -1,10 +1,13 @@
 .\build_qt.ps1
 
+# Check for UNITYPY_PATH environment variable, use default if not found
+$unityPyPath = if ($env:UNITYPY_PATH) { $env:UNITYPY_PATH } else { ".venv\Lib\site-packages\UnityPy" }
+
 pyinstaller `
     --onefile `
     --noconsole `
     --icon "./qtdesigner/images/icon.ico" `
-    --add-data ".venv\Lib\site-packages\UnityPy;UnityPy/" `
+    --add-data "$unityPyPath;UnityPy/" `
     --add-data "pages/ui;pages/ui/" `
     --hidden-import "fastparquet" `
     --name "Floowandereeze & Modding - Duel Links" `
